5bd6cb55bc50762d2398b4c2face09e35fd40ec6
MJOghbatalab <m.j.oghbatalab@gmail.com>
2024-04-16 12:55:40 +0430

    Added Student class by userr Oghbatalab

--- ConsoleApp46/Program.cs
+++ ConsoleApp46/Program.cs
@@ -1,1 +1,8 @@
 ﻿Console.WriteLine("Hi Github");
+student student = new student();
+class student
+{
+    public int id { get; set; }
+    public int name { get; set; }
+    public int family { get; set; }
+}
